a5cdb841b78e896477ccbaf258a550fea15a4581
janetkutai-sketch <janetkutai@gmail.com>
2025-09-25 17:03:29 +0300

    Add files via upload

---
+++ height,ID_number and bank_balance (1).c
@@ -0,0 +1,25 @@
+// Simple Hello World Program 
+/*
+NAME: Kutayi Janet Ayoti 
+REG NO: PA106/G/28769/25
+Description: Program to prompt the user to display their height,ID number and bank balance
+*/
+#include<stdio.h>
+
+//main function 
+int main()
+{
+    int Height; 
+    int ID_number;
+    int Bank_balance;
+    printf("Enter your height: ");
+    scanf("%d",&Height);
+    printf("your height is%d");
+    printf("Enter Your ID number: ");
+    scanf("%d",&ID_number);
+    printf("your ID number is%d");
+    printf("Enter your bank balance: ");
+    scanf("%d",&Bank_balance);
+    printf("your bank balance is%d");
+    return 0;
+}
