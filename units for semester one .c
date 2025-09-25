068823219fa0b889feda387d6fdfeb209b639e55
janetkutai-sketch <janetkutai@gmail.com>
2025-09-25 17:05:18 +0300

    Add files via upload

---
+++ units for semester one .c
@@ -0,0 +1,22 @@
+//UNITS FOR THE FIRST SEMESTER AND THEIR CODES
+
+/*
+NAME: Kutayi Janet Ayoti 
+REG NO: PA106/G/28769/25
+
+*/
+#include<stdio.h>
+
+//main function 
+int main ()
+{
+    printf("ALL UNITS FOR THE FIRST SEMESTER AND THEIR CODES\n");
+    printf("1.SPC 2101 : Introduction to programming and problem solving\n");
+    printf("2.SPI 2102 : Computer systems and organizations\n");
+    printf("3.SPM 2100 : Discrete mathematics\n");
+    printf("4.SPM 2104 : Mathematics for science\n");
+    printf("5.SSE 2101 : Fundamentals of system analysis and design\n");
+    printf("6.UCU 2105 : Fundamentals of ICT\n");
+    printf("7.UCU 2106 : Communication and information literacy skills\n");
+    return 0;
+}
