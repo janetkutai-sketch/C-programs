3b34374faa424af5bb4ece79c888775a8c04fd66
janetkutai-sketch <janetkutai@gmail.com>
2025-10-02 08:29:41 +0300

    Add files via upload

---
+++ Water_bill_calculator.C
@@ -0,0 +1,28 @@
+//
+/*
+*/
+
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill;
+
+    // Prompt the user
+    printf("Enter water units consumed: ");
+    scanf("%d", &units);
+
+    // Calculate the bill using if-else statements
+    if (units <= 30) {
+        bill = units * 20.0;
+    } else if (units <= 60) {
+        bill = (30 * 20.0) + ((units - 30) * 25.0);
+    } else {
+        bill = (30 * 20.0) + (30 * 25.0) + ((units - 60) * 30.0);
+    }
+
+    // Output the bill with 2 decimal places
+    printf("Total water bill: %.2f KES\n", bill);
+
+    return 0;
+}
