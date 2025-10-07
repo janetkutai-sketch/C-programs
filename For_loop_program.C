64de5f0d9c1b0de39e33ded6080e2eb6e82f9abd
janetkutai-sketch <janetkutai@gmail.com>
2025-10-07 20:32:43 +0300

    Add files via upload

---
+++ For_loop_program.C
@@ -0,0 +1,20 @@
+
+/*
+NAME:Kutayi Janet Ayoti
+REG NO:PA106/G/28769/25
+DESCRIPTION:For loop program to print all numbers from 100 to 50
+            in descending order
+*/
+#include <stdio.h>
+
+int main(){
+	
+	int t;
+	// start;stop;step
+	for(t=100; t>=50;t--){
+		printf("%d \n",t>=5);
+	}
+	
+	return 0;
+}
+
