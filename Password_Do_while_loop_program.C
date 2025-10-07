d42f4d2dbebd85504197e7e75aeda5221354dc26
janetkutai-sketch <janetkutai@gmail.com>
2025-10-07 20:31:28 +0300

    Add files via upload

---
+++ Password_Do_while_loop_program.C
@@ -0,0 +1,20 @@
+/*
+NAME:Kutayi Janet Ayoti
+REG NO:PA106/G/28769/25
+DESCRIPTION:
+*/
+
+ #include <stdio.h>
+ int main(){
+ 	int password;
+    
+ 	do{
+		 printf("Enter your password:");
+		 scanf("%d",& password);
+	 }while(password!=1234);
+	 
+	 printf("Access granted\n");
+			 
+ 	
+	 return 0;
+ }
